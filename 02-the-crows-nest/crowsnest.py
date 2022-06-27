a8f04b1a99f22a8083f13178f1a3ecfbc64d52d1
Alfredo Vilagut <alfredovilagutgarcia@gmail.com>
2022-06-27 15:27:05 +0100

    CrowsNest excersice done

---
+++ 02-the-crows-nest/crowsnest.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+"""
+Author : Alfredo Vilagut Garcia <alfredovilagutgarcia@gmail.com>
+Date   : 27/06/22
+Purpose: Crow's Nest -- choose the correct article
+"""
+
+import argparse
+
+
+# --------------------------------------------------
+def get_args():
+    """Get command-line arguments"""
+
+    parser = argparse.ArgumentParser(
+        description="Crow's Nest -- choose the correct article",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('word',
+                        metavar='word',
+                        help='A word')
+
+    parser.add_argument('-s',
+                        '--side',
+                        help='Choose the side of the boat',
+                        metavar='side',
+                        type=str,
+                        default='larboard')
+
+    return parser.parse_args()
+
+
+# --------------------------------------------------
+def main():
+    """Make a jazz noise here"""
+
+    args = get_args()
+    word = args.word
+    side = args.side
+    article = 'an' if word[0].lower() in 'aeiou' else 'a'
+    article = article if word[0].islower() else article.capitalize()
+    print(f'Ahoy, Captain, {article} {word} off the {side} bow!')
+
+
+# --------------------------------------------------
+if __name__ == '__main__':
+    main()
---
+++ 02-the-crows-nest/test.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+"""tests for crowsnest.py"""
+
+import os
+from subprocess import getstatusoutput, getoutput
+
+prg = 'crowsnest.py'
+consonant_words = [
+    'brigantine', 'clipper', 'dreadnought', 'frigate', 'galleon', 'haddock',
+    'junk', 'ketch', 'longboat', 'mullet', 'narwhal', 'porpoise', 'quay',
+    'regatta', 'submarine', 'tanker', 'vessel', 'whale', 'xebec', 'yatch',
+    'zebrafish'
+]
+vowel_words = ['aviso', 'eel', 'iceberg', 'octopus', 'upbound']
+template = 'Ahoy, Captain, {} {} off the larboard bow!'
+
+
+# --------------------------------------------------
+def test_exists():
+    """exists"""
+
+    assert os.path.isfile(prg)
+
+
+# --------------------------------------------------
+def test_usage():
+    """usage"""
+
+    for flag in ['-h', '--help']:
+        rv, out = getstatusoutput(f'{prg} {flag}')
+        assert rv == 0
+        assert out.lower().startswith("usage")
+
+
+# --------------------------------------------------
+def test_consonant():
+    """brigantine -> a brigantine"""
+
+    for word in consonant_words:
+        out = getoutput(f'{prg} {word}')
+        assert out.strip() == template.format('a', word)
+
+
+# --------------------------------------------------
+def test_consonant_upper():
+    """Brigantine -> A Brigatine"""
+
+    for word in consonant_words:
+        out = getoutput(f'{prg} {word.title()}')
+        assert out.strip() == template.format('A', word.title())
+
+
+# --------------------------------------------------
+def test_vowel():
+    """octopus -> an octopus"""
+
+    for word in vowel_words:
+        out = getoutput(f'{prg} {word}')
+        assert out.strip() == template.format('an', word)
+
+
+# --------------------------------------------------
+def test_vowel_upper():
+    """Octopus -> An Octopus"""
+
+    for word in vowel_words:
+        out = getoutput(f'{prg} {word.upper()}')
+        assert out.strip() == template.format('An', word.upper())
+
+
+# --------------------------------------------------
+def test_side_boat():
+    """starboard -> starboard bow!"""
+
+    for flag in ['-s starboard', '--side starboard']:
+        rv, out = getstatusoutput(f'{prg} ' + vowel_words[0] + f' {flag}')
+        assert rv == 0
+        assert out.lower().endswith("starboard bow!")
